e85dd9e4be52678eec4a6fce663b7eae75d460cb
huangqi03 <huangqi03@baidu.com>
2016-09-26 13:26:40 +0800

    fix resolve d.ts

--- lib/storage.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function clear(): Promise<{}>;
-export declare function getItem(key: string): Promise<{}>;
-export declare function removeItem(key: string): Promise<{}>;
-export declare function setItem(key: string, value: string): Promise<{}>;
