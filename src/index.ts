7a89c7d6ba6894c08524f36a8bbbf5e7fb74d324
huangqi03 <huangqi03@baidu.com>
2016-10-09 22:35:49 +0800

    我是傻子，修复一个超级傻的错误

--- src/index.ts
+++ src/index.ts
@@ -53,12 +53,12 @@ export function create(options: optionsType = {}) {
 export function mergeObservables<A extends Object, B extends Object>(target: A, source: B): A {
     let t: any = target
     let s: any = source
-    if (typeof t === 'object') {
+    if (typeof t === 'object' && typeof s === 'object') {
         for (let key in t) {
             if (t[key] && typeof t[key] === 'object' && typeof s[key] === 'object') {
-                if (isObservableMap(t[key])) return t[key].merge(s[key])
-                if (isObservableArray(t[key])) return t[key].replace(s[key])
-                if (isObservableObject(t[key])) t[key] = mergeObservables(t[key], s[key])
+                if (isObservableMap(t[key])) t[key].merge(s[key])
+                else if (isObservableArray(t[key])) t[key].replace(s[key])
+                else if (isObservableObject(t[key])) t[key] = mergeObservables(t[key], s[key])
             } else if (s[key] !== undefined) {
                 t[key] = s[key]
             }
