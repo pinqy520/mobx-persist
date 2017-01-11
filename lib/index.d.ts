df08717698b374d8276b7c2bb05b1367f0660eb1
huangqi03 <huangqi03@baidu.com>
2017-01-12 00:46:43 +0800

    rebuild

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,7 +1,8 @@
 import * as Storage from './storage';
-export declare type Types = 'object' | 'list' | 'map';
+import { Types } from './types';
 export declare function persist(type: Types, schema?: any): (target: Object, key: string, baseDescriptor?: PropertyDescriptor) => void;
 export declare function persist(target: Object, key: string, baseDescriptor?: PropertyDescriptor): void;
+export declare function persist(schema: Object): <T>(target: T) => T;
 export interface optionsType {
     storage?: Storage.IStorage | any;
 }
---
+++ lib/types.d.ts
@@ -0,0 +1,4 @@
+export declare type Types = 'object' | 'list' | 'map';
+export declare const types: {
+    [key: string]: ((s?: any) => any);
+};
