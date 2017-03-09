be73513af0f784d757141f0fe1ea199dc3d0e100
huangqi03 <huangqi03@baidu.com>
2017-03-09 09:15:48 +0800

    build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,10 +1,9 @@
-import * as Storage from './storage';
 import { Types } from './types';
 export declare function persist(type: Types, schema?: any): (target: Object, key: string, baseDescriptor?: PropertyDescriptor) => void;
 export declare function persist(target: Object, key: string, baseDescriptor?: PropertyDescriptor): void;
 export declare function persist(schema: Object): <T>(target: T) => T;
 export interface optionsType {
-    storage?: Storage.IStorage | any;
+    storage?: any;
     jsonify: boolean;
 }
 export declare function create({storage, jsonify}?: any): <T extends Object>(key: string, store: T, initialState?: any) => Promise<T>;
