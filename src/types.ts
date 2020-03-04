17c00be2b88882c27fe9bfd386121ca3845ec28d
backmeupplz <backmeupplz@gmail.com>
2020-03-04 08:57:38 -0800

    fix indentation

--- src/types.ts
+++ src/types.ts
@@ -1,34 +1,34 @@
 import {
-	list as _list,
-	map as _map,
-	object as _object,
-	date as _date,
-	custom
+    list as _list,
+    map as _map,
+    object as _object,
+    date as _date,
+    custom
 } from 'serializr'
 
 function _walk(v: any) {
-	if (typeof v === 'object' && v) Object.keys(v).map(k => _walk(v[k]))
-	return v
+    if (typeof v === 'object' && v) Object.keys(v).map(k => _walk(v[k]))
+    return v
 }
 
 function _default() {
-	return custom(_walk, (v: any) => v)
+    return custom(_walk, (v: any) => v)
 }
 
 function object(s: any) {
-	return s ? _object(s) : _default()
+    return s ? _object(s) : _default()
 }
 
 function list(s: any) {
-	return _list(object(s))
+    return _list(object(s))
 }
 
 function map(s: any) {
-	return _map(object(s))
+    return _map(object(s))
 }
 
 function date(s: any) {
-	return _date(object(s))
+    return _date(object(s))
 }
 
 export type Types = 'object' | 'list' | 'map' | 'date'
